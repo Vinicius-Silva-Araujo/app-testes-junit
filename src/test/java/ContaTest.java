12e0ec11d9039cfbb792a6e0036ce495270c35d5
Vinicius Silva Araujo <141644642+Vinicius-Silva-Araujo@users.noreply.github.com>
2024-02-19 23:42:24 -0300

    Update ContaTest.java

--- src/test/java/ContaTest.java
+++ src/test/java/ContaTest.java
@@ -12,5 +12,47 @@ public class ContaTest {
         conta.depositar(500);
         Assertions.assertEquals(500.00, conta.getSaldo());
     }
+    @Test
+    @DisplayName("Não deve Depositar com conta Inativa")
+    public void naodeveDepositarComContaInativa(){
+        Conta conta = new Conta();
+
+        conta.inativar();
+        conta.depositar(500);
+        Assertions.assertEquals(0.00, conta.getSaldo());
+    }
+    @Test
+    @DisplayName("Deve sacar com conta Ativa e Saldo Maior que o valor de saque")
+    public void deveSacarContaAtivaSaldoMaiorQSaque(){
+        Conta conta = new Conta();
+
+        conta.ativar();
+        conta.depositar(500);
+        conta.sacar(100);
+        Assertions.assertEquals(400.00, conta.getSaldo());
+    }
+    @Test
+    @DisplayName("Não pode sacar com conta Inativa")
+    public void naoSacarComCOntaInativa(){
+        Conta conta = new Conta();
+        conta.ativar();
+        conta.depositar(500);
+
+        conta.inativar();
+        conta.sacar(100);
+        Assertions.assertEquals(500.00, conta.getSaldo());
+    }
+    @Test
+    @DisplayName("Não deve sacar com conta ativa e saldo menor que valor de saque")
+    public void NaoSacarContaAtivaeSladoMenorQSaque(){
+        Conta conta = new Conta();
+        conta.ativar();
+        conta.depositar(500);
+
+        conta.inativar();
+        conta.sacar(600);
+        Assertions.assertEquals(500.00, conta.getSaldo());
+    }
+
 
 }
